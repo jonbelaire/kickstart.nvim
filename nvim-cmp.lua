3e79110509850be15b49711e47fa034db38bed65
Jon Belaire <jon.belaire@icloud.com>
2024-12-07 12:51:55 -0500

    add nvim-cmp

---
+++ nvim-cmp.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp', -- LSP source
+      'hrsh7th/cmp-buffer', -- Buffer source
+      'hrsh7th/cmp-path', -- Path source
+      'saadparwaiz1/cmp_luasnip', -- Snippet completions
+      'L3MON4D3/LuaSnip', -- Snippet engine
+    },
+    config = function()
+      local cmp = require 'cmp'
+
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert {
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm { select = true }, -- Confirm the selection
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+        }),
+      }
+    end,
+  },
+}
